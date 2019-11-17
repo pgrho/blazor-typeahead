f7b05e15bff275131349a1625e270fdce1d07b8b
pgrho <shipwreck@live.jp>
2019-11-17 21:19:10 +0900

    Add Shipwreck.BlazorTypeahead.Demo

---
+++ src/Shipwreck.BlazorTypeahead.Demo/gulpfile.js
@@ -0,0 +1,20 @@
+﻿/// <binding AfterBuild='default' Clean='clean' />
+var gulp = require("gulp");
+var del = require('del');
+
+gulp.task('clean', function () {
+    return del(['wwwroot/js', 'wwwroot/css']);
+});
+gulp.task('jquery', function () {
+    return gulp.src(['node_modules/jquery/dist/jquery.*'])
+        .pipe(gulp.dest('wwwroot/js'));
+});
+gulp.task('bootstrap', function () {
+    return gulp.src(['node_modules/bootstrap/dist/**/*'])
+        .pipe(gulp.dest('wwwroot'));
+});
+gulp.task('typeahead', function () {
+    return gulp.src(['node_modules/bootstrap-3-typeahead/bootstrap3-typeahead.*'])
+        .pipe(gulp.dest('wwwroot/js'));
+});
+gulp.task('default', gulp.series(['clean', 'jquery', 'bootstrap', 'typeahead']));
